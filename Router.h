6db92cdd46cb3a5fe2153a34e3e4672b3d291710
iveney <iveney@gmail.com>
2009-07-16 22:58:37 +0800

    Before return home

--- Router.h
+++ Router.h
@@ -74,7 +74,6 @@ struct NetRoute{
 		reach_time[0]=reach_time[1]=-1;
 		merge_time=-1;
 		timing=-1;
-		routed=false;
 	}
 	int idx;	// this net's id
 	int num_pin;    // net pins,be 2 or 3
@@ -85,7 +84,7 @@ struct NetRoute{
 	PtVector pin_route[2]; 
 	int reach_time[2]; // the time a droplet finishes routing
 	set<Point> cellset[2];
-	bool routed;    // marks if this net is routed
+	//bool routed;    // marks if this net is routed
 };
 
 // the class to stores the final routing result
